bec7345295fcd333212607eda6b49e71149a58e8
Kevin McGuire <kpmcguir@umbc.edu>
2021-02-22 11:34:10 -0500

    first commit

---
+++ events.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<!--
+    Massively by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="en">
+
+<head>
+    <title>UMBC Community Meetings</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css?v=1.0.0" />
+    <noscript>
+        <link rel="stylesheet" href="assets/css/noscript.css" />
+    </noscript>
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1045586-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "UA-1045586-1");
+    </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Monsido / www.umbc.edu Account - Installed Friday, May 15, 2020  -->
+    <script>
+    window._monsido = window._monsido || {
+        token: "firGcXTrHx9PoZBHICEsnQ",
+    };
+    </script>
+    <script async src="https://app-script.monsido.com/v2/monsido-script.js"></script>
+    <style>
+    .crt-post-name span,
+    .crt-popup-header {
+        display: none !important;
+        visibility: hidden !important;
+    }
+
+    .crt-popup-wrapper .crt-popup.has-image .crt-popup-right .crt-popup-text {
+        top: 10px !important;
+    }
+    </style>
+</head>
+
+<body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+        <!-- Intro -->
+        <!-- Header -->
+        <header id="header">
+            <img src="images/UMBC-together.png" alt="UMBC Together Logo" width="400" id="UMBC-logo" style="border: 0 !important" />
+        </header>
+        <div id="intro"></div>
+        <!-- Header -->
+        <!-- Nav -->
+        <nav id="nav">
+            <ul class="links">
+                <li><a href="index.html">#UMBCtogether</a></li>
+                <li><a href="stories.html">Stories</a></li>
+                <li class="active"><a href="events.php">Events</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="https://covid19.umbc.edu" target="_blank">COVID-19 Resources ↗</a></li>
+            </ul>
+            <ul class="icons">
+                <li>
+                    <a href="https://twitter.com/umbc" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/umbcpage" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/umbclife/" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/user/UMBCtube/" class="icon brands fa-youtube" target="_blank"><span class="label">YouTube</span></a>
+                </li>
+                <li>
+                    <a href="https://my3.my.umbc.edu/groups/umbctogether" target="_blank" style="border-bottom: 0"><img alt="" src="images/myUMBC.png" style="height: 15px" /></a>
+                </li>
+            </ul>
+        </nav>
+        <!-- Main -->
+        <div id="main">
+            <section class="post">
+                <h2>Community Events</h2>
+                <p>There are so many ways to connect virtually with your fellow Retrievers! Use our myUMBC calendar to find information about social and educational events, and be sure to participate in UMBC’s virtual community meetings scheduled throughout the semester.</p>
+                <a href="https://my3.my.umbc.edu/groups/umbctogether/events" target="_blank" class="button">
+                    View Events Feed on myUMBC
+                    <span aria-label="(opens in new window)">↗</span>
+                </a>
+                <br />
+                <hr />
+                <h2>UMBC Community Meetings</h2>
+                <p>
+                    <strong>What defines us is not whether crises happen, but how we respond to them.</strong>
+                    How is UMBC continuing to redefine excellence in higher education at the intersection of the COVID-19 pandemic, the Black Lives Matter Movement, and economic disruption?
+                    <br />
+                    <br />
+                    Faculty, staff, and students are invited to share your stories and learn together in a series of virtual gatherings during the 2020-21 academic year.
+                </p>
+
+                <header>
+                    <h3>Upcoming Virtual Community Gatherings</h3>
+                    <span class="ie right"><img alt="UMBC's True Grit statue shown with a black and gold UMBC Mask covering  his face" src="images/fall-true-grit-mask.jpg" width="300" style="float: right; margin-left: 1rem;" /></span>
+                    <p>Stay tuned for more information about upcoming community gatherings with the following themes.</p>
+                    <h4>The Empowered University: Balancing Inclusion and Free Speech</h4>
+                    <p>Wednesday, January 27<br>
+                    4pm, via Webex</p>
+                </header>
+
+                
+                <p>
+                <span style="font-style: italic;">
+                What is our role in protecting free speech as a public university?<br>
+                What should I do if I see or hear content that does not align with my values?<br>
+                What is the difference between free speech and hate speech?<br>
+                Is hate speech protected?<br>
+                Can I be disciplined for my speech online? What about in a classroom?<br><br></span>
+                
+                Help us start a community discussion about the challenges of balancing our shared commitments to free speech and equity and inclusion.<br>
+                
+                Please share your thoughts and questions when you register for the event. <br>
+                
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLScz0MMtwai6nERJp1qUZ0uj4VN6ijkLEgx_zOQsPARd8PHPPA/viewform"><strong>Registration link <span class="sr-only">For The Empowered University: Balancing Inclusion and Free Speech Event</span></strong></a><br>
+                
+                <strong><a href="https://umbc.webex.com/mw3300/mywebex/default.do?nomenu=true&siteurl=umbc&service=6&rnd=0.10835614147688055&main_url=https%3A%2F%2Fumbc.webex.com%2Fec3300%2Feventcenter%2Fevent%2FeventAction.do%3FtheAction%3Ddetail%26%26%26EMK%3D4832534b000000048b1e29f6b79fe71f456d031a972b2cbfecb444e54d53c6ccfe125a48b2bb765b%26siteurl%3Dumbc%26confViewID%3D183903250981879939%26encryptTicket%3DSDJTSwAAAAQBDK7Rof-bSRIGuxUoNbK1tQMPxrrm8fVvL0-ehYpSQA2%26" target="_blank">Webex link <span class="sr-only">For The Empowered University: Balancing Inclusion and Free Speech Event</span></a></strong></p>
+                
+                <hr />
+                <h3>Previous Virtual Community Gatherings</h3>
+                <header>
+                    <h4>The Empowered University</h4>
+                    <p>
+                        <strong>Shared Leadership, Culture Change, and Academic Success in a Pivotal Time</strong>
+                        <br />
+                        The Annual UMBC Fall Opening Meeting
+                        <br />
+                        Thursday, August 20
+                        <br />
+                        <a href="https://www.youtube.com/playlist?list=PLnj_pHJHgqkUFhZdmxvqg1t58Uf4VOS1A" target="_blank">Watch the recorded event here.</a>
+                    </p>
+
+                    <h4>Truth, Racial Healing and Transformation in Our Communities Town Hall</h4>
+                    <p>
+                        Thursday, September 24
+                        <br />
+                        <a href="https://youtu.be/_B99W7OIIaM" target="_blank">Watch the recorded event here.</a>
+                    </p>
+
+                    <h4>The Empowered University: Internationalization and Global Engagement</h4>
+
+                    <p>
+                        Tuesday, October 27
+                        <br />
+                        <a href="https://umbc.app.box.com/s/ncobp2d3i6l3w77whjg361rwkl4vi7dw" target="_blank">
+                            Watch the recorded event
+                            <span class="sr-only">of The Empowered University: Internationalization and Global Engagement</span>
+                            here.
+                        </a>
+                        <br />
+                        Due to a technical error, while closed captioning was provided live, it is not available for this file, we apologize for the inconvenience.
+                        <br />
+                        <a href="assets/pdf/internationalization-and-global-engagement-transcript.pdf" target="_blank">
+                            You can view a time stamped transcript
+                            <span class="sr-only">of The Empowered University: Internationalization and Global Engagement</span>
+                            here. (PDF File)
+                        </a>
+                        <br />
+                        Future town halls will continue to be closed captioned.
+                    </p>
+
+                    <h4>The Empowered University: Teaching and Learning</h4>
+                    <p>
+                        Wednesday, November 18<br>
+
+                        <a href="https://drive.google.com/file/d/11A4423Yf-uvZY0LSPbUM710BcjbkqKLn/view">
+                            Watch the recorded event
+
+                            <span class="sr-only">The Empowered University: Teaching and Learning
+                            </span>
+                            here
+                        </a>
+
+                    </p>
+                    
+                    <h4>Grit & Gratitude - Virtual Holiday Celebration</h4>
+                    
+                    <p>
+                        Wednesday, December 16
+                        
+                        <br>
+                        <a href="https://youtu.be/9ln0_btqyA0" target="_blank">
+                        Watch the recorded event <span class="sr-only">Grit & Gratitude - Virtual Holiday Celebration</span> here 
+                        </a>
+                        
+                    </p>
+                </header>
+            </section>
+        </div>
+        <!-- Footer -->
+        <footer id="footer">
+            <section>
+                <h2 style="font-size: 1.2rem !important">Join the conversation on social with #UMBCtogether, or suggest a link or post to include in our feeds:</h2>
+                <form action="https://formspree.io/xlewqdva" method="POST">
+                    <div class="fields">
+                        <div class="field">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div class="field">
+                            <label for="email">Email</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div class="field">
+                            <label for="url">Link URL</label>
+                            <input type="text" name="url" id="url" />
+                        </div>
+                        <div class="field">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <ul class="actions">
+                        <li><input type="submit" value="Send Message" /></li>
+                    </ul>
+                </form>
+            </section>
+            <section class="split contact">
+                <section class="alt">
+                    <h3>UMBC</h3>
+                    <p>
+                        1000 Hilltop Circle
+                        <br />
+                        Baltimore, MD 21250
+                    </p>
+                </section>
+                <section>
+                    <h3>Phone</h3>
+                    <p><a href="tel:4104551000">410-455-1000</a></p>
+                </section>
+                <section>
+                    <h3>Email</h3>
+                    <p><a href="mailto:together@umbc.edu">together@umbc.edu</a></p>
+                </section>
+                <section>
+                    <h3>Social</h3>
+                    <ul class="icons alt">
+                        <li>
+                            <a href="https://my3.my.umbc.edu/groups/umbctogether" target="_blank" style="border-bottom: 0"><img src="images/myUMBC.png" alt="myUMBC  Logo" style="height: 15px; padding-right: 10px" /></a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/umbc" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/umbcpage" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/umbclife/" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/user/UMBCtube/" class="icon brands fa-youtube" target="_blank"><span class="label">YouTube</span></a>
+                        </li>
+                    </ul>
+                </section>
+            </section>
+        </footer>
+        <!-- Copyright -->
+        <div id="copyright">
+            <img src="images/umbc-transparent.png" alt="UMBC" width="310" id="Footer-UMBC-logo" />
+            <br />
+            <br />
+            <ul>
+                <li>&copy; UMBC Together</li>
+                <li>
+                    Web Framework by:
+                    <a href="https://html5up.net">HTML5 UP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
